7f876505dfa329522843e8b793d72a34ba2b7f32
CRUZ CRUZ JULIO CESAR <julio.cruzc@ine.mx>
2026-05-24 02:27:26 -0600

    feat: selector de entidades federativas en campo Estado

--- public/js/views/nueva.js
+++ public/js/views/nueva.js
@@ -78,7 +78,41 @@ function renderNueva() {
 
             <div class="field-group">
               <label>Estado</label>
-              <input type="text" name="autoridad_estado" placeholder="Estado">
+              <select name="autoridad_estado">
+                <option value="">— Seleccionar estado —</option>
+                <option>Aguascalientes</option>
+                <option>Baja California</option>
+                <option>Baja California Sur</option>
+                <option>Campeche</option>
+                <option>Chiapas</option>
+                <option>Chihuahua</option>
+                <option>Ciudad de México</option>
+                <option>Coahuila de Zaragoza</option>
+                <option>Colima</option>
+                <option>Durango</option>
+                <option>Estado de México</option>
+                <option>Guanajuato</option>
+                <option>Guerrero</option>
+                <option>Hidalgo</option>
+                <option>Jalisco</option>
+                <option>Michoacán de Ocampo</option>
+                <option>Morelos</option>
+                <option>Nayarit</option>
+                <option>Nuevo León</option>
+                <option>Oaxaca</option>
+                <option>Puebla</option>
+                <option>Querétaro</option>
+                <option>Quintana Roo</option>
+                <option>San Luis Potosí</option>
+                <option>Sinaloa</option>
+                <option>Sonora</option>
+                <option>Tabasco</option>
+                <option>Tamaulipas</option>
+                <option>Tlaxcala</option>
+                <option>Veracruz de Ignacio de la Llave</option>
+                <option>Yucatán</option>
+                <option>Zacatecas</option>
+              </select>
             </div>
 
             <div class="field-group">
